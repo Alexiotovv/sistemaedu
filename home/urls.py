9e92c7391d519bc34e295e39e0e5c3dab21dd03b
Alexiotovv <gpsalexvasquez@gmail.com>
2024-07-04 20:14:58 -0500

    ruta

--- home/urls.py
+++ home/urls.py
@@ -4,7 +4,7 @@ from .views import *
 from django.contrib.auth.decorators import login_required
 urlpatterns = [
     #path('', login_required(index),name='home'),
-    path('public_home', public_home,name='public_home'),
+    path('', public_home,name='public_home'),
     path('home', home,name='home'),
     path("login/", login, name="login"),
     path("login_estudiante/", login_estudiante, name="login_estudiante"),
